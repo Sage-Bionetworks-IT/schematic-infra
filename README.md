868045891bcece2e4b6c3a2099fb5d0ca7166c5c
Khai Do <3697686+zaro0508@users.noreply.github.com>
2024-11-04 13:19:06 -0800

    add files from Sage-Bionetworks-IT/openchallenges (#1)
    We are basically using openchallenges as a template for this repo

--- README.md
+++ README.md
@@ -1,2 +1,319 @@
-# schematic-infra
-AWS infrastructure for Schematic
+
+# OpenChallenges AWS CDK app
+
+AWS CDK app for deploying [OpenChallenges](https://openchallenges.io/).
+
+# Perequisites
+
+AWS CDK projects require some bootstrapping before synthesis or deployment.
+Please review the [bootstapping documentation](https://docs.aws.amazon.com/cdk/v2/guide/getting_started.html#getting_started_bootstrap)
+before development.
+
+# Dev Container
+
+This repository provides a [dev container](https://containers.dev/) that includes all the tools
+required to develop this AWS CDK app.
+
+## Opening the project inside its dev container
+
+With VS Code:
+
+1. Clone this repo
+2. File > Open Folder...
+3. A prompt should invite you to open the project inside the dev container. If not, open VS Code
+    Command Palette and select "Dev Containers: Open Folder in Container..."
+
+With GitHub Codespaces:
+
+1. From the main page of this repository, click on the button "Code" > Codespaces > Click on the
+   button "Create codespace"
+
+That's it! You are now inside the dev container and have access to all the development tools.
+
+# Development
+
+All the development tools are provided when developing inside the dev container
+(see above). These tools include Python, AWS CLI, AWS CDK CLI, etc. These tools
+also include a Python virtual environment where all the Python packages needed
+are already installed.
+
+If you decide the develop outside of the dev container, some of the development
+tools can be installed by running:
+
+```console
+./tools/setup.sh
+```
+
+Development requires the activation of the Python virtual environment:
+
+```
+$ source .venv/bin/activate
+```
+
+At this point you can now synthesize the CloudFormation template for this code.
+
+```
+$ cdk synth
+```
+
+To add additional dependencies, for example other CDK libraries, just add
+them to your `setup.py` file and rerun the `pip install -r requirements.txt`
+command.
+
+## Useful commands
+
+ * `cdk ls`          list all stacks in the app
+ * `cdk synth`       emits the synthesized CloudFormation template
+ * `cdk deploy`      deploy this stack to your default AWS account/region
+ * `cdk diff`        compare deployed stack with current state
+ * `cdk docs`        open CDK documentation
+
+
+# Testing
+
+## Static Analysis
+
+As a pre-deployment step we syntatically validate the CDK json, yaml and
+python files with [pre-commit](https://pre-commit.com).
+
+Please install pre-commit, once installed the file validations will
+automatically run on every commit.  Alternatively you can manually
+execute the validations by running `pre-commit run --all-files`.
+
+Verify CDK to Cloudformation conversion by running [cdk synth]:
+
+```console
+cdk synth
+```
+
+The Cloudformation output is saved to the `cdk.out` folder
+
+## Unit Tests
+
+Tests are available in the tests folder. Execute the following to run tests:
+
+```
+python -m pytest tests/ -s -v
+```
+
+
+# Environments
+
+Deployment context is set in the [cdk.json](cdk.json) file.  An `ENV` environment variable must be
+set to tell the CDK which environment's variables to use when synthesising or deploying the stacks.
+
+Set an environment in cdk.json in `context` section of cdk.json:
+
+```json
+  "context": {
+    "dev": {
+        "VPC_CIDR": "10.255.92.0/24",
+        "FQDN": "dev.openchallenges.io"
+    },
+    "prod": {
+        "VPC_CIDR": "10.255.94.0/24",
+        "FQDN": "prod.openchallenges.io"
+    },
+  }
+```
+
+For example, using the `prod` environment:
+
+```console
+ENV=prod cdk synth
+```
+
+# Certificates
+
+Certificates to set up HTTPS connections should be created manually in AWS certificate manager.
+This is not automated due to AWS requiring manual verification of the domain ownership.
+Once created take the ARN of the certificate and add it to a context in cdk.json.
+
+```json
+  "context": {
+    "dev": {
+      "CERTIFICATE_ARN": "arn:aws:acm:us-east-1:XXXXXXXXX:certificate/76ed5a71-4aa8-4cc1-9db6-aa7a322ec077"
+    }
+  }
+```
+
+![ACM certificate](docs/acm-certificate.png)
+
+# Secrets
+
+Secrets can be stored in one of the following locations:
+  * AWS SSM parameter store
+  * Local context in [cdk.json](cdk.json) file
+
+## Loading directly from cdk.json
+
+Set secrets directly in cdk.json in `context` section of cdk.json:
+
+```text
+  "context": {
+    "secrets": {
+        "MARIADB_PASSWORD": "Dummy",
+        "MARIADB_ROOT_PASSWORD": "Dummy",
+        "GIT_HOST_KEY": "Host123",
+        "GIT_PRIVATE_KEY": "-----BEGIN OPENSSH PRIVATE KEY-----\nDUMMY_GIT_PRIVATE_KEY\n-----END OPENSSH PRIVATE KEY-----",
+        "AWS_LOADER_S3_ACCESS_KEY_ID": "AccessKey123",
+        "AWS_LOADER_S3_SECRET_ACCESS_KEY": "SecretAccessKey123",
+        "SECURITY_KEY": "SecurityKey123"
+    }
+  }
+```
+
+## Loading from ssm parameter store
+
+Set secrets to the SSM parameter names in `context` section of cdk.json:
+
+```text
+  "context": {
+    "secrets": {
+        "MARIADB_PASSWORD": "/openchallenges/MARIADB_PASSWORD",
+        "MARIADB_ROOT_PASSWORD": "/openchallenges/MARIADB_ROOT_PASSWORD",
+        "GIT_HOST_KEY": "/openchallenges/GIT_HOST_KEY",
+        "GIT_PRIVATE_KEY": "/openchallenges/GIT_PRIVATE_KEY",
+        "AWS_LOADER_S3_ACCESS_KEY_ID": "/openchallenges/AWS_LOADER_S3_ACCESS_KEY_ID",
+        "AWS_LOADER_S3_SECRET_ACCESS_KEY": "/openchallenges/AWS_LOADER_S3_SECRET_ACCESS_KEY",
+        "SECURITY_KEY": "/openchallenges/SECURITY_KEY"
+    }
+  }
+```
+
+where the values of these KVs (e.g. `/openchallenges/MARIADB_PASSWORD`) refer to SSM parameters that
+must be created manually.
+
+![AWS secrets manager](docs/aws-parameter-store.png)
+
+## Specify secret location
+
+Set the `SECRETS` environment variable to specify the location where secrets should be loaded from.
+
+Load secrets directly from cdk.json file:
+
+```console
+SECRETS=local cdk synth
+```
+
+Load secrets from AWS SSM parameter store:
+
+```console
+AWS_PROFILE=<your-aws-profile> AWS_DEFAULT_REGION=us-east-1 SECRETS=ssm cdk synth
+```
+
+> [!NOTE]
+> Setting `SECRETS=ssm` requires access to an AWS account
+
+## Override secrets from command line
+
+The CDK CLI allows overriding context variables:
+
+To load secrets directly from passed in values:
+
+```console
+SECRETS=local cdk --context  secrets='{"MARIADB_PASSWORD": "Dummy", "MARIADB_ROOT_PASSWORD": "Dummy", ..}' synth
+```
+
+To load secrets from SSM parameter store with overridden SSM parameter names:
+
+```console
+SECRETS=ssm cdk --context  "secrets"='{"MARIADB_PASSWORD": "/test/mariadb-root-pass", "MARIADB_ROOT_PASSWORD": "/test/mariadb-root-pass", ..}' synth
+```
+
+# Deployment
+
+## Bootstrap
+
+There are a few items that need to be manually bootstrapped before deploying the
+OpenChallenges application.
+
+* Add OC [secrets](#Secrets) to either the cdk.json or the AWS System Manager parameter store
+* Create an [ACM certificate for the application](#Certificates) using the AWS Certificates Manager
+* Add the Certificate ARN to the cdk.json
+* Update references to the OC docker images in [app.py](app.py)
+  (i.e. `ghcr.io/sage-bionetworks/openchallenges-xxx:<tag>`)
+* (Optional) Update the `ServiceProps` objects in [app.py](app.py) with parameters specific to
+  each container.
+
+## Login with the AWS CLI
+
+> [!NOTE]
+> This and the following sections assume that you are working in the AWS account
+> `org-sagebase-itsandbox` with the role `Developer` and that you are deploying
+> to the `us-east-1` region. If this assumption is correct, you should be able
+> to simply copy-paste the following commands, otherwise adapting the
+> configuration should be straightforward.
+
+Create the config file if it doesn't exist yet.
+
+```console
+mkdir ~/.aws && touch ~/.aws/config
+```
+
+As a Developer working in Sage IT Sandbox AWS account, add the following profile to the config file.
+
+```ini
+[profile itsandbox-dev]
+sso_start_url = https://d-906769aa66.awsapps.com/start
+sso_region = us-east-1
+sso_account_id = XXXXXXXXX
+sso_role_name = Developer
+```
+
+Login with the AWS CLI:
+
+```console
+aws --profile itsandbox-dev sso login
+```
+
+
+## Deploy
+
+Deployment requires setting up an [AWS profile](https://docs.aws.amazon.com/cli/latest/userguide/getting-started-quickstart.html) then executing the
+following command:
+
+```console
+AWS_PROFILE=itsandbox-dev AWS_DEFAULT_REGION=us-east-1 ENV=dev SECRETS=ssm cdk deploy --all
+```
+
+## Force new deployment
+
+```console
+AWS_PROFILE=itsandbox-dev AWS_DEFAULT_REGION=us-east-1 aws ecs update-service \
+  --cluster <cluster-name> \
+  --service <service-name> \
+  --force-new-deployment
+```
+
+# Execute a command from a container running on ECS
+
+Once a container has been deployed successfully it is accessible for debugging using the
+[ECS execute-command](https://docs.aws.amazon.com/cli/latest/reference/ecs/execute-command.html)
+
+Example to get an interactive shell run into a container:
+
+```console
+AWS_PROFILE=itsandbox-dev AWS_DEFAULT_REGION=us-east-1 aws ecs execute-command \
+  --cluster OpenChallengesEcs-ClusterEB0386A7-BygXkQgSvdjY \
+  --task a2916461f65747f390fd3e29f1b387d8 \
+  --container openchallenges-mariadb \
+  --command "/bin/sh" --interactive
+```
+
+
+# CI Workflow
+
+This repo has been set up to use Github Actions CI to continously deploy the
+OpenChallenges app.
+
+The workflow for continuous integration:
+
+* Create PR from the git dev branch
+* PR is reviewed and approved
+* PR is merged
+* CI deploys changes to the dev environment (dev.openchallenges.io) in the org-sagebase-openchallenges-dev account.
+* Changes are promoted (or merged) to the git stage branch.
+* CI deploys changes to the staging environment (stage.openchallenges.io) in the org-sagebase-openchallenges-prod account.
+* Changes are promoted (or merged) to the git prod branch.
+* CI deploys changes to the prod environment (prod.openchallenges.io) in the org-sagebase-openchallenges-prod account.
---
+++ app.py
@@ -0,0 +1,374 @@
+import aws_cdk as cdk
+
+from openchallenges.bucket_stack import BucketStack
+from openchallenges.network_stack import NetworkStack
+from openchallenges.ecs_stack import EcsStack
+from openchallenges.service_stack import ServiceStack
+from openchallenges.service_stack import LoadBalancedServiceStack
+from openchallenges.load_balancer_stack import LoadBalancerStack
+from openchallenges.service_props import ServiceProps
+import openchallenges.utils as utils
+
+app = cdk.App()
+
+# get the environment
+environment = utils.get_environment()
+stack_name_prefix = f"openchallenges-{environment}"
+image_version = "0.0.11"
+
+# get VARS from cdk.json
+env_vars = app.node.try_get_context(environment)
+fully_qualified_domain_name = env_vars["FQDN"]
+subdomain, domain = fully_qualified_domain_name.split(".", 1)
+vpc_cidr = env_vars["VPC_CIDR"]
+certificate_arn = env_vars["CERTIFICATE_ARN"]
+
+# get secrets from cdk.json or aws parameter store
+secrets = utils.get_secrets(app)
+
+bucket_stack = BucketStack(app, f"{stack_name_prefix}-buckets")
+
+network_stack = NetworkStack(app, f"{stack_name_prefix}-network", vpc_cidr)
+
+ecs_stack = EcsStack(
+    app, f"{stack_name_prefix}-ecs", network_stack.vpc, fully_qualified_domain_name
+)
+ecs_stack.add_dependency(network_stack)
+
+mariadb_props = ServiceProps(
+    "openchallenges-mariadb",
+    3306,
+    512,
+    f"ghcr.io/sage-bionetworks/openchallenges-mariadb:{image_version}",
+    {
+        "MARIADB_USER": "maria",
+        "MARIADB_PASSWORD": secrets["MARIADB_PASSWORD"],
+        "MARIADB_ROOT_PASSWORD": secrets["MARIADB_ROOT_PASSWORD"],
+    },
+)
+
+mariadb_stack = ServiceStack(
+    app,
+    f"{stack_name_prefix}-mariadb",
+    network_stack.vpc,
+    ecs_stack.cluster,
+    mariadb_props,
+)
+
+elasticsearch_props = ServiceProps(
+    "openchallenges-elasticsearch",
+    9200,
+    2048,
+    f"ghcr.io/sage-bionetworks/openchallenges-elasticsearch:{image_version}",
+    {
+        "bootstrap.memory_lock": "true",
+        "discovery.type": "single-node",  # https://stackoverflow.com/a/68253868
+        "JAVA_TOOL_OPTIONS": "-XX:InitialHeapSize=1g -XX:MaxHeapSize=1g",
+    },
+)
+
+elasticsearch_stack = ServiceStack(
+    app,
+    f"{stack_name_prefix}-elasticsearch",
+    network_stack.vpc,
+    ecs_stack.cluster,
+    elasticsearch_props,
+)
+
+thumbor_props = ServiceProps(
+    "openchallenges-thumbor",
+    8889,
+    512,
+    f"ghcr.io/sage-bionetworks/openchallenges-thumbor:{image_version}",
+    {
+        "LOG_LEVEL": "info",
+        "PORT": "8889",
+        "LOADER": "thumbor_aws.loader",
+        "AWS_LOADER_REGION_NAME": "us-east-1",
+        "AWS_LOADER_BUCKET_NAME": bucket_stack.openchallenges_img_bucket.bucket_name,
+        "AWS_LOADER_S3_ENDPOINT_URL": "http://s3.us-east-1.amazonaws.com",
+        "AWS_LOADER_ROOT_PATH": "img",
+        "STORAGE": "thumbor.storages.file_storage",
+        "FILE_STORAGE_ROOT_PATH": "/data/storage",
+        "RESULT_STORAGE": "thumbor.result_storages.file_storage",
+        "RESULT_STORAGE_FILE_STORAGE_ROOT_PATH": "/data/result_storage",
+        "RESULT_STORAGE_STORES_UNSAFE": "True",
+        "RESULT_STORAGE_EXPIRATION_SECONDS": "2629746",
+        "SECURITY_KEY": secrets["SECURITY_KEY"],
+        "ALLOW_UNSAFE_URL": "True",
+        "QUALITY": "100",
+        "MAX_AGE": "86400",
+        "AUTO_PNG_TO_JPG": "True",
+        "HTTP_LOADER_VALIDATE_CERTS": "False",
+    },
+)
+
+thumbor_stack = ServiceStack(
+    app,
+    f"{stack_name_prefix}-thumbor",
+    network_stack.vpc,
+    ecs_stack.cluster,
+    thumbor_props,
+)
+thumbor_stack.add_dependency(bucket_stack)
+
+config_server_props = ServiceProps(
+    "openchallenges-config-server",
+    8090,
+    1024,
+    f"ghcr.io/sage-bionetworks/openchallenges-config-server:{image_version}",
+    {
+        "GIT_DEFAULT_LABEL": "test-2",
+        "GIT_HOST_KEY_ALGORITHM": "ssh-ed25519",
+        "GIT_HOST_KEY": secrets["GIT_HOST_KEY"],
+        "GIT_PRIVATE_KEY": secrets["GIT_PRIVATE_KEY"],
+        "GIT_URI": "git@github.com:Sage-Bionetworks/openchallenges-config-server-repository.git",
+        "SERVER_PORT": "8090",
+    },
+)
+
+config_server_stack = ServiceStack(
+    app,
+    f"{stack_name_prefix}-config-server",
+    network_stack.vpc,
+    ecs_stack.cluster,
+    config_server_props,
+)
+
+service_registry_props = ServiceProps(
+    "openchallenges-service-registry",
+    8081,
+    1024,
+    f"ghcr.io/sage-bionetworks/openchallenges-service-registry:{image_version}",
+    {
+        "SERVER_PORT": "8081",
+        "DEFAULT_ZONE": "http://localhost:8081/eureka",
+        "SPRING_CLOUD_CONFIG_URI": "http://openchallenges-config-server:8090",
+    },
+)
+
+service_registry_stack = ServiceStack(
+    app,
+    f"{stack_name_prefix}-service-registry",
+    network_stack.vpc,
+    ecs_stack.cluster,
+    service_registry_props,
+)
+service_registry_stack.add_dependency(config_server_stack)
+
+zipkin_props = ServiceProps(
+    "openchallenges-zipkin",
+    9411,
+    512,
+    f"ghcr.io/sage-bionetworks/openchallenges-zipkin:{image_version}",
+    {},
+)
+
+zipkin_stack = ServiceStack(
+    app,
+    f"{stack_name_prefix}-zipkin",
+    network_stack.vpc,
+    ecs_stack.cluster,
+    zipkin_props,
+)
+
+image_service_props = ServiceProps(
+    "openchallenges-image-service",
+    8086,
+    1024,
+    f"ghcr.io/sage-bionetworks/openchallenges-image-service:{image_version}",
+    {
+        "SERVER_PORT": "8086",
+        "SPRING_CLOUD_CONFIG_URI": "http://openchallenges-config-server:8090",
+        "SERVICE_REGISTRY_URL": "http://openchallenges-service-registry:8081/eureka",
+        "OPENCHALLENGES_IMAGE_SERVICE_THUMBOR_HOST": f"https://{fully_qualified_domain_name}/img/",
+        "OPENCHALLENGES_IMAGE_SERVICE_THUMBOR_SECURITY_KEY": secrets["SECURITY_KEY"],
+        "OPENCHALLENGES_IMAGE_SERVICE_IS_DEPLOYED_ON_AWS": "true",
+    },
+)
+
+image_service_stack = ServiceStack(
+    app,
+    f"{stack_name_prefix}-image-service",
+    network_stack.vpc,
+    ecs_stack.cluster,
+    image_service_props,
+)
+image_service_stack.add_dependency(service_registry_stack)
+image_service_stack.add_dependency(thumbor_stack)
+image_service_stack.add_dependency(zipkin_stack)
+
+challenge_service_props = ServiceProps(
+    "openchallenges-challenge-service",
+    8085,
+    1024,
+    f"ghcr.io/sage-bionetworks/openchallenges-challenge-service:{image_version}",
+    {
+        "SERVER_PORT": "8085",
+        "SPRING_CLOUD_CONFIG_URI": "http://openchallenges-config-server:8090",
+        "SERVICE_REGISTRY_URL": "http://openchallenges-service-registry:8081/eureka",
+        "KEYCLOAK_URL": "http://openchallenges-keycloak:8080",
+        "SPRING_DATASOURCE_USERNAME": "maria",
+        "SPRING_DATASOURCE_PASSWORD": secrets["MARIADB_PASSWORD"],
+        "DB_URL": "jdbc:mysql://openchallenges-mariadb:3306/challenge_service?allowLoadLocalInfile=true",
+        "DB_PLATFORMS_CSV_PATH": "/workspace/BOOT-INF/classes/db/platforms.csv",
+        "DB_CHALLENGES_CSV_PATH": "/workspace/BOOT-INF/classes/db/challenges.csv",
+        "DB_CONTRIBUTION_ROLES_CSV_PATH": "/workspace/BOOT-INF/classes/db/contribution_roles.csv",
+        "DB_INCENTIVES_CSV_PATH": "/workspace/BOOT-INF/classes/db/incentives.csv",
+        "DB_INPUT_DATA_TYPE_CSV_PATH": "/workspace/BOOT-INF/classes/db/input_data_type.csv",
+        "DB_SUBMISSION_TYPES_CSV_PATH": "/workspace/BOOT-INF/classes/db/submission_types.csv",
+        "DB_CATEGORIES_CSV_PATH": "/workspace/BOOT-INF/classes/db/categories.csv",
+        "DB_EDAM_CONCEPT_CSV_PATH": "/workspace/BOOT-INF/classes/db/edam_concept.csv",
+        "OPENCHALLENGES_CHALLENGE_SERVICE_IS_DEPLOYED_ON_AWS": "true",
+    },
+)
+
+challenge_service_stack = ServiceStack(
+    app,
+    f"{stack_name_prefix}-challenge-service",
+    network_stack.vpc,
+    ecs_stack.cluster,
+    challenge_service_props,
+)
+challenge_service_stack.add_dependency(service_registry_stack)
+challenge_service_stack.add_dependency(mariadb_stack)
+challenge_service_stack.add_dependency(elasticsearch_stack)
+challenge_service_stack.add_dependency(zipkin_stack)
+
+
+organization_service_props = ServiceProps(
+    "openchallenges-organization-service",
+    8084,
+    1024,
+    f"ghcr.io/sage-bionetworks/openchallenges-organization-service:{image_version}",
+    {
+        "SERVER_PORT": "8084",
+        "SPRING_CLOUD_CONFIG_URI": "http://openchallenges-config-server:8090",
+        "KEYCLOAK_URL": "http://openchallenges-keycloak:8080",
+        "SERVICE_REGISTRY_URL": "http://openchallenges-service-registry:8081/eureka",
+        "SPRING_DATASOURCE_USERNAME": "maria",
+        "SPRING_DATASOURCE_PASSWORD": secrets["MARIADB_PASSWORD"],
+        "DB_URL": "jdbc:mysql://openchallenges-mariadb:3306/organization_service?allowLoadLocalInfile=true",
+        "DB_ORGANIZATIONS_CSV_PATH": "/workspace/BOOT-INF/classes/db/organizations.csv",
+        "DB_CONTRIBUTION_ROLES_CSV_PATH": "/workspace/BOOT-INF/classes/db/contribution_roles.csv",
+        "OPENCHALLENGES_ORGANIZATION_SERVICE_IS_DEPLOYED_ON_AWS": "true",
+    },
+)
+
+organization_service_stack = ServiceStack(
+    app,
+    f"{stack_name_prefix}-organization-service",
+    network_stack.vpc,
+    ecs_stack.cluster,
+    organization_service_props,
+)
+organization_service_stack.add_dependency(image_service_stack)
+organization_service_stack.add_dependency(mariadb_stack)
+organization_service_stack.add_dependency(elasticsearch_stack)
+organization_service_stack.add_dependency(zipkin_stack)
+
+api_gateway_props = ServiceProps(
+    "openchallenges-api-gateway",
+    8082,
+    1024,
+    f"ghcr.io/sage-bionetworks/openchallenges-api-gateway:{image_version}",
+    {
+        "SERVER_PORT": "8082",
+        "SPRING_CLOUD_CONFIG_URI": "http://openchallenges-config-server:8090",
+        "SERVICE_REGISTRY_URL": "http://openchallenges-service-registry:8081/eureka",
+        "KEYCLOAK_URL": "http://openchallenges-keycloak:8080",
+        "OPENCHALLENGES_API_GATEWAY_IS_DEPLOYED_ON_AWS": "true",
+    },
+)
+
+api_gateway_stack = ServiceStack(
+    app,
+    f"{stack_name_prefix}-api-gateway",
+    network_stack.vpc,
+    ecs_stack.cluster,
+    api_gateway_props,
+)
+api_gateway_stack.add_dependency(service_registry_stack)
+
+oc_app_props = ServiceProps(
+    "openchallenges-app",
+    4200,
+    1024,
+    f"ghcr.io/sage-bionetworks/openchallenges-app:{image_version}",
+    {
+        "API_DOCS_URL": f"https://{fully_qualified_domain_name}/api-docs",
+        "APP_VERSION": "1.0.0-alpha",
+        "CSR_API_URL": f"https://{fully_qualified_domain_name}/api/v1",
+        "DATA_UPDATED_ON": "2024-10-11",
+        "ENVIRONMENT": "production",
+        "GOOGLE_TAG_MANAGER_ID": "GTM-NBR5XD8C",
+        "SSR_API_URL": "http://openchallenges-api-gateway:8082/api/v1",
+    },
+)
+
+oc_app_stack = ServiceStack(
+    app, f"{stack_name_prefix}-app", network_stack.vpc, ecs_stack.cluster, oc_app_props
+)
+oc_app_stack.add_dependency(organization_service_stack)
+oc_app_stack.add_dependency(api_gateway_stack)
+oc_app_stack.add_dependency(challenge_service_stack)
+oc_app_stack.add_dependency(image_service_stack)
+
+# From AWS docs https://docs.aws.amazon.com/AmazonECS/latest/developerguide/service-connect-concepts-deploy.html
+# The public discovery and reachability should be created last by AWS CloudFormation, including the frontend
+# client service. The services need to be created in this order to prevent an time period when the frontend
+# client service is running and available the public, but a backend isn't.
+load_balancer_stack = LoadBalancerStack(
+    app, f"{stack_name_prefix}-load-balancer", network_stack.vpc
+)
+
+api_docs_props = ServiceProps(
+    "openchallenges-api-docs",
+    8010,
+    256,
+    f"ghcr.io/sage-bionetworks/openchallenges-api-docs:{image_version}",
+    {"PORT": "8010"},
+)
+api_docs_stack = ServiceStack(
+    app,
+    f"{stack_name_prefix}-api-docs",
+    network_stack.vpc,
+    ecs_stack.cluster,
+    api_docs_props,
+)
+
+apex_service_props = ServiceProps(
+    "openchallenges-apex",
+    8000,
+    200,
+    f"ghcr.io/sage-bionetworks/openchallenges-apex:{image_version}",
+    {
+        "API_DOCS_HOST": "openchallenges-api-docs",
+        "API_DOCS_PORT": "8010",
+        "API_GATEWAY_HOST": "openchallenges-api-gateway",
+        "API_GATEWAY_PORT": "8082",
+        "APP_HOST": "openchallenges-app",
+        "APP_PORT": "4200",
+        "THUMBOR_HOST": "openchallenges-thumbor",
+        "THUMBOR_PORT": "8889",
+        "ZIPKIN_HOST": "openchallenges-zipkin",
+        "ZIPKIN_PORT": "9411",
+    },
+)
+
+apex_service_stack = LoadBalancedServiceStack(
+    app,
+    f"{stack_name_prefix}-apex",
+    network_stack.vpc,
+    ecs_stack.cluster,
+    apex_service_props,
+    load_balancer_stack.alb,
+    certificate_arn,
+    health_check_path="/health",
+    health_check_interval=5,
+)
+apex_service_stack.add_dependency(oc_app_stack)
+apex_service_stack.add_dependency(api_docs_stack)
+apex_service_stack.add_dependency(load_balancer_stack)
+
+app.synth()
